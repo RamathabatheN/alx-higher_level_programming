1abb92b2f53f187984359ea64c2b3efb78214e6c
Ramathabathe.N <mashoto23@gmail.com>
2023-12-04 07:13:38 -0800

    if else pyhton

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if last digit > 5:
+    print("f{number}and is greater than 5")
+if last digit == 0:
+    print("f{number}and is 0")
+if last digit < 6 and != 0:
+    print("f{number} and is less than 6 and not 0")
